c174ae325fc125032737e7251aa94435a4b1fdc7
Yzquile <deadnoct@gmail.com>
2023-12-21 23:48:43 +0800

    Fixed the slider animation bug, added index contents

--- app.js
+++ app.js
@@ -6,16 +6,31 @@ let dots = document.querySelectorAll('.slider .dots li');
 
 let lengthItems = items.length - 1;
 let active = 0;
-next.onclick = function(){
+next.onclick = function (event) {
+    clearInterval(refreshInterval);
     active = active + 1 <= lengthItems ? active + 1 : 0;
-    reloadSlider();
-}
-prev.onclick = function(){
+    reloadSlider(event);
+    refreshInterval = setInterval(() => next.click(), 3000);
+};
+
+prev.onclick = function (event) {
+    clearInterval(refreshInterval);
     active = active - 1 >= 0 ? active - 1 : lengthItems;
-    reloadSlider();
-}
-let refreshInterval = setInterval(()=> {next.click()}, 3000);
-function reloadSlider(){
+    reloadSlider(event);
+    refreshInterval = setInterval(() => next.click(), 3000);
+};
+
+dots.forEach((li, key) => {
+    li.addEventListener('click', (event) => {
+        clearInterval(refreshInterval);
+        active = key;
+        reloadSlider(event);
+        refreshInterval = setInterval(() => next.click(), 3000);
+    });
+});
+
+let refreshInterval = setInterval(() => { next.click() }, 3000);
+function reloadSlider() {
     slider.style.left = -items[active].offsetLeft + 'px';
     // 
     let last_active_dot = document.querySelector('.slider .dots li.active');
@@ -23,17 +38,29 @@ function reloadSlider(){
     dots[active].classList.add('active');
 
     clearInterval(refreshInterval);
-    refreshInterval = setInterval(()=> {next.click()}, 3000);
+    refreshInterval = setInterval(() => { next.click() }, 3000);
 
-    
+
 }
 
 dots.forEach((li, key) => {
-    li.addEventListener('click', ()=>{
-         active = key;
-         reloadSlider();
+    li.addEventListener('click', () => {
+        active = key;
+        reloadSlider();
     })
 })
-window.onresize = function(event) {
+window.onresize = function (event) {
     reloadSlider();
 };
+
+function reloadSlider() {
+    slider.style.left = -items[active].offsetLeft + 'px';
+
+    let last_active_dot = document.querySelector('.slider .dots li.active');
+    last_active_dot.classList.remove('active');
+    dots[active].classList.add('active');
+
+    // Prevent the default behavior of the click event
+    event.preventDefault();
+}
+
