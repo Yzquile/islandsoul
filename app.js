e10f981003566db3c7a708ef92da700830f258d4
Yzquile <deadnoct@gmail.com>
2023-12-29 00:59:56 +0800

    added modal on Credits footer link and page under maintenance

--- app.js
+++ app.js
@@ -79,11 +79,11 @@ toggleBtn.onclick = function () {
     toggleBtnIcon.classList = isOpen
         ? 'fa-solid fa-xmark'
         : 'fa-solid fa-bars';
-}; 
+};
 
 
 // JS for about.html video popup
-$(function() {
+$(function () {
     $('.popup-youtube, .popup-vimeo').magnificPopup({
         disableOn: 700,
         type: 'iframe',
@@ -93,3 +93,38 @@ $(function() {
         fixedContentPos: false
     });
 });
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myCredit");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+function openModal() {
+    // Add your modal opening logic here
+    document.getElementById('myModal').style.display = 'block';
+  }
+
+  function closeModal() {
+    // Add your modal closing logic here
+    document.getElementById('myModal').style.display = 'none';
+  }
